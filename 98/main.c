6a7c1911891a847d7a70394ae6c4edf837db3b7a
Muaykak <sutawith001@gmail.com>
2023-07-09 22:05:36 +0700

    nothing

---
+++ 98/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf(" Write a C program that accepts some text from the user and prints each word of that text on a separate line.\n\n");
+	printf("Input some text:\n");
+	int flag=0;
+	char input[100];
+	scanf('%c')
+	
+}
