8a02657c5b8cc3397061013b27bb181dfcfcd490
Muaykak <sutawith001@gmail.com>
2023-07-10 20:50:47 +0700

    works for today

--- 98/main.c
+++ 98/main.c
@@ -3,8 +3,20 @@
 int main(){
 	printf(" Write a C program that accepts some text from the user and prints each word of that text on a separate line.\n\n");
 	printf("Input some text:\n");
-	int flag=0;
+	int flag=0,count=0,i=0;
 	char input[100];
-	scanf('%c')
-	
+	char *c;
+	scanf("%[^\n]", input);
+	do {
+		c = &input[i];
+		if(*c != ' ' && *c != '\t'){
+		printf("%c", *c);
+		}
+		else {
+			printf("\n");
+		}
+		i++;
+	} while (*c != '\0');
+
+	return 0;	
 }
